ffce3979443466fe1202c748bb05e716e9adf887
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-06 20:19:38 -0700

    Add testing capability to the postgres db Configure testing environment in Application extension and configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,12 +8,22 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let databaseName: String
+    let databasePort: Int
+    if (app.environment == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = "vapor_database"
+        databasePort = 5432
+    }
+    
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        port: databasePort,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        database: Environment.get("DATABASE_NAME") ?? databaseName
     ), as: .psql)
     
     app.migrations.add(CreateProject())

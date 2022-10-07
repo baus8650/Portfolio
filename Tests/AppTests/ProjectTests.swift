f48b5ba777a59f3121735531d69b600169e84130
Tim Bausch <49855076+baus8650@users.noreply.github.com>
2022-10-06 20:20:17 -0700

    Add tests for CRUD methods in Projects, Skills, and Projects+Skills Remove boilerplate AppTests file

---
+++ Tests/AppTests/ProjectTests.swift
@@ -0,0 +1,105 @@
+@testable import App
+import XCTVapor
+
+final class ProjectTests: XCTestCase {
+    let testProject1 = ProjectBuilder().build()
+    let testProject2 = ProjectBuilder().name("Second Project").yearCompleted(2021).build()
+    let projectsURI = "/api/projects/"
+    let skillsURI = "/api/skills/"
+    var app: Application!
+    
+    override func setUpWithError() throws {
+        app = try Application.testable()
+    }
+    
+    override func tearDownWithError() throws {
+        app.shutdown()
+    }
+    
+    func testProjectsCanBeRetrievedFromAPI() throws {
+        let project1 = Project(name: testProject1.name, yearCompleted: testProject1.yearCompleted)
+        try project1.save(on: app.db).wait()
+        
+        let project2 = Project(name: testProject2.name, yearCompleted: testProject2.yearCompleted)
+        try project2.save(on: app.db).wait()
+            
+        try app.test(.GET, projectsURI, afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+            
+            let projects = try response.content.decode([Project].self)
+            XCTAssertEqual(projects.count, 2)
+            XCTAssertEqual(projects[0].name, "Test Project")
+            XCTAssertEqual(projects[0].yearCompleted, 2022)
+            XCTAssertEqual(projects[0].id, project1.id)
+            XCTAssertEqual(projects[1].name, "Second Project")
+            XCTAssertEqual(projects[1].yearCompleted, 2021)
+            XCTAssertEqual(projects[1].id, project2.id)
+            
+        })
+    }
+    
+    func testProjectCanBeSavedWithAPI() throws {
+        let project = Project(id: testProject1.id, name: testProject1.name, yearCompleted: testProject1.yearCompleted)
+        try app.test(.POST, projectsURI, beforeRequest: { req in
+            try req.content.encode(project)
+        }, afterResponse: { response in
+            let receivedProject = try response.content.decode(Project.self)
+            XCTAssertEqual(receivedProject.name, testProject1.name)
+            XCTAssertEqual(receivedProject.yearCompleted, testProject1.yearCompleted)
+            XCTAssertEqual(receivedProject.id, testProject1.id)
+            
+            try app.test(.GET, projectsURI, afterResponse: { secondResponse in
+                let projects = try secondResponse.content.decode([Project].self)
+                XCTAssertEqual(projects.count, 1)
+                XCTAssertEqual(projects[0].name, "Test Project")
+                XCTAssertEqual(projects[0].yearCompleted, 2022)
+                XCTAssertEqual(projects[0].id, testProject1.id)
+            })
+        })
+    }
+    
+    func testUpdatingAProject() throws {
+        let project1 = Project(name: testProject1.name, yearCompleted: testProject1.yearCompleted)
+        try project1.save(on: app.db).wait()
+        
+        let newName = "Edited Project Name"
+        let updatedProjectData = Project(id: project1.id, name: newName, yearCompleted: project1.yearCompleted)
+        
+        try app.test(.GET, "\(projectsURI)\(project1.id!)", afterResponse: { response in
+            let returnedProject = try response.content.decode(Project.self)
+            XCTAssertEqual(returnedProject.name, project1.name)
+            XCTAssertEqual(returnedProject.yearCompleted, project1.yearCompleted)
+        })
+        
+        try app.test(.PUT, "\(projectsURI)\(project1.id!)", beforeRequest: { request in
+            try request.content.encode(updatedProjectData)
+        })
+        
+        try app.test(.GET, "\(projectsURI)\(project1.id!)", afterResponse: { response in
+            let returnedProject = try response.content.decode(Project.self)
+            XCTAssertEqual(returnedProject.name, newName)
+            XCTAssertEqual(returnedProject.yearCompleted, project1.yearCompleted)
+        })
+    }
+    
+    func testProjectCanBeDeletedWithAPI() throws {
+        let project1 = Project(id: testProject1.id, name: testProject1.name, yearCompleted: testProject1.yearCompleted)
+        try project1.save(on: app.db).wait()
+        
+        let project2 = Project(id: testProject2.id, name: testProject2.name, yearCompleted: testProject2.yearCompleted)
+        try project2.save(on: app.db).wait()
+        
+        try app.test(.GET, projectsURI, afterResponse: { response in
+            let projects = try response.content.decode([Project].self)
+            XCTAssertEqual(projects.count, 2)
+        })
+        
+        try app.test(.DELETE, "\(projectsURI)\(project1.id!)")
+        
+        try app.test(.GET, projectsURI, afterResponse: { response in
+            let projects = try response.content.decode([Project].self)
+            XCTAssertEqual(projects.count, 1)
+            XCTAssertEqual(projects[0].name, "Second Project")
+        })
+    }
+}
